8a99c5a4e8a9f183780a669800b78c04418aa384
Rafael Almeida <supremotop10@gmail.com>
2021-05-26 01:33:15 -0300

    Problema para comparar as datas, falta inserir a os indicadores de aumento e queda dos valores diários

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,64 @@
 const [confirmedE, deathE, recoveredE, activeE, comboE, todayE] = document.querySelectorAll("#confirmed, #death, #recovered, #active, #combo, #today");
 
-///total/country/${country}
+window.addEventListener('load', () => {
+    loadApplication();
+})
 
 //Função para facilitar a consulta com fetch
 function fetchE(url) {
     return fetch(`https://api.covid19api.com/${url}`).then(res => res.json());
 }
 
-async function queryAPI() {
+async function loadApplication() {
     await Promise.all([fetchE("summary"), fetchE("countries")])
         .then(([summary, countries]) => {
-            populateIndicator(countries, summary);
+            populateInitialIndicator(countries, summary);
         })
-
 }
 
-queryAPI();
+function populateInitialIndicator(countries, summary) {
+    populateComboCountries(countries);
+    populateSummary(summary);
+    todayE.value = formatDate(new Date());
+}
 
-async function queryAPICountries(country) {
-    await Promise.all([fetchE(`country/${country}`)])
-        .then(countries => {
-            return countries;
-        });
+const queryAPICountries = async (country) => {
+    return await fetchE(`country/${country}`);
 }
 
 function populateSummary(summary) {
 
-    let summaryG = summary.Global;
+    let confirmed;
+    let death;
+    let recovered;
+    let active;
 
-    let confirmed = summaryG.TotalConfirmed;
-    let death = summaryG.TotalDeaths;
-    let recovered = summaryG.TotalRecovered;
-    let active = confirmed - (death + recovered);
+    if (summary.Global) {
+        let summaryG = summary.Global;
+        confirmed = summaryG.TotalConfirmed;
+        death = summaryG.TotalDeaths;
+        recovered = summaryG.TotalRecovered;
+        active = confirmed - (death + recovered);
+    } else {
+        confirmed = summary.Confirmed;
+        death = summary.Deaths;
+        recovered = summary.Recovered;
+        active = confirmed - (death + recovered);
+    }
 
     confirmedE.innerHTML = formatNumber(confirmed);
     deathE.innerHTML = formatNumber(death);
     recoveredE.innerHTML = formatNumber(recovered);
     activeE.innerHTML = formatNumber(active);
-
-    //Data
-    todayE.value = formatDate(summaryG.Date);
 }
 
-function populateSummaryGlobal(countries) {
+//melhorar calculo de ativos
+function sumActive(confirmed, death, recovered) {
+    if (death <= 0) {
+
+    }
+}
+function populateComboCountries(countries) {
 
     countries.map((country) => {
         let optionT = document.createElement('option');
@@ -52,6 +68,37 @@ function populateSummaryGlobal(countries) {
         comboE.appendChild(optionT);
     });
 }
+
+// Evento no combobox
+comboE.addEventListener("change", async () => {
+    await queryAPICountries(comboE.value)
+        .then(res => {
+            res.map(result => {
+                if (formatDate(result.Date) === todayE.value) {
+                    console.log(result.Date)
+                    populateSummary(result);
+                }
+            })
+        })
+});
+
+// 1 Segundo para captura do que foi digitado ou clicado
+todayE.addEventListener('change', () => {
+    setTimeout(async () => {
+        await queryAPICountries(comboE.value)
+            .then(res => {
+                res.map(result => {
+                    if (formatDate(Date.parse(result.Date)) == Date.parse(todayE.value)) {
+                        console.log(formatDate(result.Date))
+                        console.log((todayE.value))
+                        console.log(typeof (formatDate(result.Date)))
+                        console.log(typeof (todayE.value))
+                        populateSummary(result.Date);
+                    }
+                })
+            })
+    }, 1000)
+});
 
 function formatNumber(number) {
     return number.toLocaleString("pt-BR");
@@ -68,16 +115,5 @@ function formatDate(date) {
     if (day.length < 2)
         day = '0' + day;
 
-    return [year, month, day].join('-');
+    return [year, month, day].join('-').toString();
 }
-
-function populateIndicator(countries, summary) {
-    populateSummaryGlobal(countries);
-    populateSummary(summary);
-}
-
-comboE.addEventListener("change", () => {
-    queryAPICountries('thailand');
-    console.log(val);
-    todayE.value
-});
